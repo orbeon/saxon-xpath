d39244949dce29d54757d09420529ea2f5dba37b
Erik Bruchez <erik@bruchez.org>
2020-08-13 16:33:05 -0700

    Minimal test and move to ScalaTest

--- build.sbt
+++ build.sbt
@@ -2,6 +2,10 @@ import sbtcrossproject.CrossPlugin.autoImport.{crossProject, CrossType}
 
 enablePlugins(ScalaJSPlugin)
 
+val ScalatTestVersion = "3.2.1"
+
+traceLevel in ThisBuild := 0
+
 lazy val saxon = (crossProject(JVMPlatform, JSPlatform).crossType(CrossType.Full) in file("saxon"))
   .settings(
     organization := "org.orbeon",
@@ -10,12 +14,14 @@ lazy val saxon = (crossProject(JVMPlatform, JSPlatform).crossType(CrossType.Full
 
     scalaVersion := "2.13.1",
 
-    libraryDependencies += "com.lihaoyi"   %%% "utest"       % "0.7.2" % Test,
-    libraryDependencies += "org.jetbrains" % "annotations"   % "17.0.0",
-    libraryDependencies += "com.ibm.icu"   % "icu4j"         % "63.1", // Java  only
-    libraryDependencies += "xml-resolver"  % "xml-resolver"  % "1.2",   // Java  only
+    libraryDependencies += "org.scalactic" %%% "scalactic"     % ScalatTestVersion    % Test,
+    libraryDependencies += "org.scalatest" %%% "scalatest"     % ScalatTestVersion    % Test,
 
-    testFrameworks += new TestFramework("utest.runner.Framework")
+    libraryDependencies += "org.jetbrains" %   "annotations"   % "17.0.0",
+    libraryDependencies += "com.ibm.icu"   %   "icu4j"         % "63.1", // Java  only
+    libraryDependencies += "xml-resolver"  %   "xml-resolver"  % "1.2",  // Java  only
+
+    testOptions       in Test          += Tests.Argument(TestFrameworks.ScalaTest, "-oF")
   )
 
 lazy val saxonJS = saxon.js
--- saxon/shared/src/test/scala/net/sf/saxon/XPathTest.scala
+++ saxon/shared/src/test/scala/net/sf/saxon/XPathTest.scala
@@ -1,19 +1,34 @@
 package net.sf.saxon
 
-import net.sf.saxon.s9api.XdmAtomicValue
-import utest._
+import net.sf.saxon.s9api.{XdmAtomicValue, XdmItem}
 
-object XPathTest extends TestSuite {
+import org.scalatest.funspec.AnyFunSpec
 
-  def tests = Tests {
-    test("Minimalistic expression compilation and execution") {
-      val p = new s9api.Processor
-      val c = p.newXPathCompiler()
-      val executable = c.compile("42")
-      val selector = executable.load()
-      selector.setContextItem(new XdmAtomicValue(1))
-      val result = selector.evaluateSingle()
-      println(s"result `${result.toString}`")
-    }
+class XPathTest extends AnyFunSpec {
+
+  def compileAndRunExpression(xpath: String): XdmItem = {
+    val p = new s9api.Processor
+    val c = p.newXPathCompiler()
+    val executable = c.compile(xpath)
+    val selector = executable.load()
+    selector.setContextItem(new XdmAtomicValue(1))
+    selector.evaluateSingle()
+  }
+
+  describe("Minimalistic expression compilation and execution") {
+
+    val Expected = List(
+      "42"     → "42",
+      "42 + 1" → "43",
+//      "3.1415" → "43",
+      "'To be, or not to be, that is the question'" → "To be, or not to be, that is the question",
+//      "concat('To be', ', or not to be')" → "To be, or not to be",
+//      "'To be' || ', or not to be'" → "To be, or not to be",
+    )
+
+    for ((in, out) ← Expected)
+      it(s"must evaluate `$in`") {
+        assert(out == compileAndRunExpression(in).toString)
+      }
   }
 }
