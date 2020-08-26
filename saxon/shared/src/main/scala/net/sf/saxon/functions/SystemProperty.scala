c31032bd7ba18bd3acb419b6625094349f843196
Erik Bruchez <erik@bruchez.org>
2020-08-25 17:37:37 -0700

    Refactor: use `case` and fix `==`

--- saxon/shared/src/main/scala/net/sf/saxon/functions/SystemProperty.scala
+++ saxon/shared/src/main/scala/net/sf/saxon/functions/SystemProperty.scala
@@ -30,49 +30,39 @@ object SystemProperty {
 
   def yesOrNo(whatever: Boolean): String = if (whatever) "yes" else "no"
 
-  def getProperty(uri: String,
-                  local: String,
-                  rsc: RetainedStaticContext): String = {
-    val config: Configuration = rsc.getConfiguration
-    val edition: String = rsc.getPackageData.getTargetEdition
+  def getProperty(uri: String, local: String, rsc: RetainedStaticContext): String = {
+    val config = rsc.getConfiguration
+    val edition = rsc.getPackageData.getTargetEdition
     if (uri == NamespaceConstant.XSLT) {
       local match {
-        case "version" => return "3.0"
-        case "vendor" => Version.getProductVendor
-        case "vendor-url" => Version.getWebSiteAddress
-        case "product-name" => Version.getProductName
-        case "product-version" => Version.getProductVariantAndVersion(edition)
-        case "is-schema-aware" =>
-          var schemaAware: Boolean = rsc.getPackageData.isSchemaAware
-          yesOrNo(schemaAware)
-        case "supports-serialization" => yesOrNo("JS" != edition)
-        case "supports-backwards-compatibility" => return "yes"
-        case "supports-namespace-axis" => return "yes"
+        case "version"                          => "3.0"
+        case "vendor"                           => Version.getProductVendor
+        case "vendor-url"                       => Version.getWebSiteAddress
+        case "product-name"                     => Version.getProductName
+        case "product-version"                  => Version.getProductVariantAndVersion(edition)
+        case "is-schema-aware"                  => yesOrNo(rsc.getPackageData.isSchemaAware)
+        case "supports-serialization"           => yesOrNo("JS" != edition)
+        case "supports-backwards-compatibility" => "yes"
+        case "supports-namespace-axis"          => "yes"
         case "supports-streaming" =>
           yesOrNo(
             "EE" == edition &&
-              config.isLicensedFeature(
-                Configuration.LicenseFeature.ENTERPRISE_XSLT) &&
-              config.getConfigurationProperty(Feature.STREAMABILITY).!=("off"))
-        case "supports-dynamic-evaluation" =>
-          yesOrNo(!config.getBooleanProperty(Feature.DISABLE_XSL_EVALUATE))
-        case "supports-higher-order-functions" => return "yes"
-        case "xpath-version" => return "3.1"
-        case "xsd-version" =>
-          if (rsc.getConfiguration.getXsdVersion == Configuration.XSD10) return "1.0"
-          else return "1.1"
-
+              config.isLicensedFeature(Configuration.LicenseFeature.ENTERPRISE_XSLT) &&
+              config.getConfigurationProperty(Feature.STREAMABILITY) != "off"
+          )
+        case "supports-dynamic-evaluation"      => yesOrNo(!config.getBooleanProperty(Feature.DISABLE_XSL_EVALUATE))
+        case "supports-higher-order-functions"  => "yes"
+        case "xpath-version"                    => "3.1"
+        case "xsd-version"                      => if (rsc.getConfiguration.getXsdVersion == Configuration.XSD10) "1.0" else "1.1"
+        case _                                  => ""
       }
-      ""
-    } else if (uri.isEmpty &&
-      config.getBooleanProperty(Feature.ALLOW_EXTERNAL_FUNCTIONS)) {
-      val `val`: String = System.getProperty(local)
+    } else if (uri.isEmpty && config.getBooleanProperty(Feature.ALLOW_EXTERNAL_FUNCTIONS)) {
+      val `val` = System.getProperty(local)
       if (`val` == null) "" else `val`
     } else {
       ""
     }
   }
-
 }
 
 class SystemProperty extends SystemFunction with Callable {
@@ -95,20 +85,18 @@ class SystemProperty extends SystemFunction with Callable {
         val uri: String = qName.getURI
         val local: String = qName.getLocalPart
         if (uri == NamespaceConstant.XSLT &&
-          (local.==("version") || local.==("vendor") || local.==(
-            "vendor-url") ||
-            local.==("product-name") ||
-            local.==("product-version") ||
-            local.==("supports-backwards-compatibility") ||
-            local.==("xpath-version") ||
-            local.==("xsd-version"))) {
+          (local == "version" || local == "vendor" || local == "vendor-url" ||
+            local == "product-name" ||
+            local == "product-version" ||
+            local == "supports-backwards-compatibility" ||
+            local == "xpath-version" ||
+            local == "xsd-version")) {
           val result: String =
             getProperty(uri, local, getRetainedStaticContext)
           new StringLiteral(result)
         }
       } catch {
-        case e: XPathException => {}
-
+        case _: XPathException => // no action
       }
     }
     null
@@ -136,12 +124,12 @@ class SystemProperty extends SystemFunction with Callable {
     val uri: String = qName.getURI
     val local: String = qName.getLocalPart
     uri == NamespaceConstant.XSLT &&
-      (local.==("version") || local.==("vendor") || local.==("vendor-url") ||
-        local.==("product-name") ||
-        local.==("product-version") ||
-        local.==("supports-backwards-compatibility") ||
-        local.==("xpath-version") ||
-        local.==("xsd-version"))
+      (local == "version" || local == "vendor" || local == "vendor-url" ||
+        local == "product-name" ||
+        local == "product-version" ||
+        local == "supports-backwards-compatibility" ||
+        local == "xpath-version" ||
+        local == "xsd-version")
   }
 
 }
