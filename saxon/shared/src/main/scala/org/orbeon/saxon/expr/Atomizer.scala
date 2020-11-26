2f92c2941ca8b20ceca60525fa48b3723966fdc4
Erik Bruchez <erik@bruchez.org>
2020-11-25 19:18:41 -0800

    Fix missing `return`s and cleanup

--- saxon/shared/src/main/scala/org/orbeon/saxon/expr/Atomizer.scala
+++ saxon/shared/src/main/scala/org/orbeon/saxon/expr/Atomizer.scala
@@ -1,101 +1,66 @@
 package org.orbeon.saxon.expr
 
 import org.orbeon.saxon.event.ReceiverOption
-
-import org.orbeon.saxon.expr.instruct.Block
-
-import org.orbeon.saxon.expr.instruct.Choose
-
-import org.orbeon.saxon.expr.instruct.ValueOf
-
+import org.orbeon.saxon.expr.Atomizer._
+import org.orbeon.saxon.expr.instruct.{Block, Choose, ValueOf}
 import org.orbeon.saxon.expr.parser._
-
 import org.orbeon.saxon.functions.Error
-
 import org.orbeon.saxon.ma.arrays.ArrayItemType
-
 import org.orbeon.saxon.ma.map.MapType
-
 import org.orbeon.saxon.model._
-
 import org.orbeon.saxon.om._
-
-import org.orbeon.saxon.pattern.NameTest
-
-import org.orbeon.saxon.pattern.NodeKindTest
-
-import org.orbeon.saxon.pattern.NodeTest
-
+import org.orbeon.saxon.pattern.{NameTest, NodeKindTest, NodeTest}
 import org.orbeon.saxon.trace.ExpressionPresenter
-
 import org.orbeon.saxon.trans.XPathException
-
-import org.orbeon.saxon.tree.iter.AtomizingIterator
-
-import org.orbeon.saxon.tree.iter.EmptyIterator
-
-import org.orbeon.saxon.tree.iter.UntypedAtomizingIterator
-
-import org.orbeon.saxon.value.AtomicValue
-
-import org.orbeon.saxon.value.Cardinality
-
-import org.orbeon.saxon.value.EmptySequence
-
-import java.util.EnumSet
-
-import Atomizer._
+import org.orbeon.saxon.tree.iter.{AtomizingIterator, EmptyIterator, UntypedAtomizingIterator}
+import org.orbeon.saxon.value.{AtomicValue, Cardinality, EmptySequence}
 
 import scala.beans.{BeanProperty, BooleanBeanProperty}
 
-//remove if not needed
-//import scala.collection.compat._
-import scala.jdk.CollectionConverters._
 
 object Atomizer {
 
   def makeAtomizer(sequence: Expression, role: RoleDiagnostic): Expression =
-    if (sequence.isInstanceOf[Literal] &&
-      sequence.asInstanceOf[Literal].getValue.isInstanceOf[AtomicSequence]) {
-      sequence
-    } else {
-      new Atomizer(sequence, role)
+    sequence match {
+      case literal: Literal if literal.getValue.isInstanceOf[AtomicSequence] =>
+        sequence
+      case _ =>
+        new Atomizer(sequence, role)
     }
 
   def getAtomizedItemType(operand: Expression,
                           alwaysUntyped: Boolean,
                           th: TypeHierarchy): ItemType = {
-    val in: ItemType = operand.getItemType
+    val in = operand.getItemType
     if (in.isPlainType) {
-      return in
-    } else if (in.isInstanceOf[NodeTest]) {
-      val kinds: UType = in.getUType
-      if (alwaysUntyped) {
-        if (STRING_KINDS.subsumes(kinds)) {
-          BuiltInAtomicType.STRING
-        }
-        if (UNTYPED_IF_UNTYPED_KINDS.subsumes(kinds)) {
-          BuiltInAtomicType.UNTYPED_ATOMIC
-        }
-      } else {
-        if (UNTYPED_KINDS.subsumes(kinds)) {
-          BuiltInAtomicType.UNTYPED_ATOMIC
-        }
+      in
+    } else
+      in match {
+        case _: NodeTest =>
+          val kinds = in.getUType
+          if (alwaysUntyped) {
+            if (STRING_KINDS.subsumes(kinds))
+              return BuiltInAtomicType.STRING
+            if (UNTYPED_IF_UNTYPED_KINDS.subsumes(kinds))
+              return BuiltInAtomicType.UNTYPED_ATOMIC
+          } else {
+            if (UNTYPED_KINDS.subsumes(kinds))
+              return BuiltInAtomicType.UNTYPED_ATOMIC
+          }
+          in.getAtomizedItemType
+        case _: JavaExternalObjectType =>
+          in.getAtomizedItemType
+        case arrayItemType: ArrayItemType =>
+          val ait: PlainType = arrayItemType
+            .getMemberType
+            .getPrimaryType
+            .getAtomizedItemType
+          if (ait == null) ErrorType else ait
+        case _: FunctionItemType =>
+          ErrorType
+        case _ =>
+          BuiltInAtomicType.ANY_ATOMIC
       }
-      in.getAtomizedItemType
-    } else if (in.isInstanceOf[JavaExternalObjectType]) {
-      in.getAtomizedItemType
-    } else if (in.isInstanceOf[ArrayItemType]) {
-      val ait: PlainType = in
-        .asInstanceOf[ArrayItemType]
-        .getMemberType
-        .getPrimaryType
-        .getAtomizedItemType
-      if (ait == null) return ErrorType else return ait
-    } else if (in.isInstanceOf[FunctionItemType]) {
-      ErrorType
-    }
-    BuiltInAtomicType.ANY_ATOMIC
   }
 
   val STRING_KINDS: UType =
@@ -112,34 +77,32 @@ object Atomizer {
                            oneToOne: Boolean): SequenceIterator = {
     val properties: Set[SequenceIterator.Property.Property] = base.getProperties
     if (properties.contains(SequenceIterator.Property.LAST_POSITION_FINDER)) {
-      val count: Int = base.asInstanceOf[LastPositionFinder].getLength
+      val count = base.asInstanceOf[LastPositionFinder].getLength
       if (count == 0) {
-        EmptyIterator.emptyIterator
+        return EmptyIterator.emptyIterator
       } else if (count == 1) {
-        val first: Item = base.next()
-        first.atomize().iterate()
+        val first = base.next()
+        return first.atomize().iterate()
       }
     } else if (properties.contains(SequenceIterator.Property.ATOMIZING)) {
-      new AxisAtomizingIterator(base.asInstanceOf[AtomizedValueIterator])
+      return new AxisAtomizingIterator(base.asInstanceOf[AtomizedValueIterator])
     }
-    if (oneToOne) {
+    if (oneToOne)
       new UntypedAtomizingIterator(base)
-    } else {
+    else
       new AtomizingIterator(base)
-    }
   }
 
   def atomize(sequence: Sequence): AtomicSequence =
-    if (sequence.isInstanceOf[AtomicSequence]) {
-      sequence.asInstanceOf[AtomicSequence]
-    } else if (sequence.isInstanceOf[EmptySequence[_ <: Item]]) {
-      EmptyAtomicSequence.getInstance
-    } else {
-      val iter: SequenceIterator =
-        getAtomizingIterator(sequence.iterate(), oneToOne = false)
-      new AtomicArray(iter)
+    sequence match {
+      case atomicSequence: AtomicSequence =>
+        atomicSequence
+      case _: EmptySequence[_] =>
+        EmptyAtomicSequence.getInstance
+      case _ =>
+        val iter = getAtomizingIterator(sequence.iterate(), oneToOne = false)
+        new AtomicArray(iter)
     }
-
 }
 
 class Atomizer(sequence: Expression, role: RoleDiagnostic)
@@ -157,89 +120,78 @@ class Atomizer(sequence: Expression, role: RoleDiagnostic)
 
   sequence.setFlattened(true)
 
-  def getOperandRole(): OperandRole = OperandRole.ATOMIC_SEQUENCE
+  def getOperandRole: OperandRole = OperandRole.ATOMIC_SEQUENCE
 
   def getImplementationMethod: Int = Expression.ITERATE_METHOD | Expression.WATCH_METHOD
 
-  def setRoleDiagnostic(role: RoleDiagnostic): Unit = {
+  def setRoleDiagnostic(role: RoleDiagnostic): Unit =
     this.roleDiagnostic = role
-  }
 
   override def simplify(): Expression = {
-    untyped = !getPackageData.isSchemaAware
+    untyped = ! getPackageData.isSchemaAware
     computeSingleValued(getConfiguration.getTypeHierarchy)
-    val operand: Expression = getBaseExpression.simplify()
-    if (operand.isInstanceOf[Literal]) {
-      val `val`: GroundedValue = operand.asInstanceOf[Literal].getValue
-      if (`val`.isInstanceOf[AtomicValue]) {
-        return operand
-      }
-      val iter: SequenceIterator = `val`.iterate()
-      var i: Item = null
-      while (({
-        i = iter.next()
-        i
-      }) != null) {
-        if (i.isInstanceOf[NodeInfo]) {
-          return this
-        }
-        if (i.isInstanceOf[Function]) {
-          if (i.asInstanceOf[Function].isArray) {
-            this
-          } else if (i.asInstanceOf[Function].isMap) {
-            val err = new XPathException(
-              expandMessage(
-                "Cannot atomize a map (" + i.toShortString + ")"),
-              "FOTY0013")
-            err.setIsTypeError(true)
-            err.setLocation(getLocation)
-            throw err
-          } else {
-            val err = new XPathException(
-              expandMessage("Cannot atomize a function item"),
-              "FOTY0013")
-            err.setIsTypeError(true)
-            err.setLocation(getLocation)
-            throw err
+    val operand = getBaseExpression.simplify()
+    operand match {
+      case literal: Literal =>
+        val `val` = literal.getValue
+        if (`val`.isInstanceOf[AtomicValue])
+          return operand
+        val iter = `val`.iterate()
+        var i: Item = null
+        while ({
+          i = iter.next()
+          i
+        } != null) {
+          i match {
+            case _: NodeInfo =>
+              return this
+            case function: Function =>
+              if (function.isArray) {
+                return this
+              } else if (function.isMap) {
+                val err = new XPathException(expandMessage("Cannot atomize a map (" + i.toShortString + ")"), "FOTY0013")
+                err.setIsTypeError(true)
+                err.setLocation(getLocation)
+                throw err
+              } else {
+                val err = new XPathException(expandMessage("Cannot atomize a function item"), "FOTY0013")
+                err.setIsTypeError(true)
+                err.setLocation(getLocation)
+                throw err
+              }
+            case _ =>
           }
         }
-      }
-      return operand
-    } else if (operand.isInstanceOf[ValueOf] &&
-      !ReceiverOption.contains(
-        operand.asInstanceOf[ValueOf].getOptions,
-        ReceiverOption.DISABLE_ESCAPING)) {
-      operand.asInstanceOf[ValueOf].convertToCastAsString
+        operand
+      case of: ValueOf if ! ReceiverOption.contains(of.getOptions, ReceiverOption.DISABLE_ESCAPING) =>
+        of.convertToCastAsString
+      case _ =>
+        this.setBaseExpression(operand)
+        this
     }
-    this.setBaseExpression(operand)
-    this
   }
 
   override def typeCheck(visitor: ExpressionVisitor,
                          contextInfo: ContextItemStaticInfo): Expression = {
     getOperand.typeCheck(visitor, contextInfo)
-    untyped = untyped |
-      !visitor.getStaticContext.getPackageData.isSchemaAware
+    untyped = untyped | ! visitor.getStaticContext.getPackageData.isSchemaAware
     val th = visitor.getConfiguration.getTypeHierarchy
     computeSingleValued(th)
     resetLocalStaticProperties()
-    val operandType: ItemType = getOperandItemType
-    if (th.isSubType(operandType, BuiltInAtomicType.ANY_ATOMIC)) {
-      getBaseExpression
-    }
-    if (!operandType.isAtomizable(th)) {
-      var err: XPathException = null
-      if (operandType.isInstanceOf[FunctionItemType]) {
-        val thing: String =
-          if (operandType.isInstanceOf[MapType]) "map" else "function item"
-        err = new XPathException(expandMessage("Cannot atomize a " + thing),
-          "FOTY0013")
-      } else {
-        err = new XPathException(
-          expandMessage(
-            "Cannot atomize an element that is defined in the schema to have element-only content"),
-          "FOTY0012")
-      }
+    val operandType = getOperandItemType
+    if (th.isSubType(operandType, BuiltInAtomicType.ANY_ATOMIC))
+      return getBaseExpression
+    if (! operandType.isAtomizable(th)) {
+      val err =
+        if (operandType.isInstanceOf[FunctionItemType]) {
+          val thing = if (operandType.isInstanceOf[MapType]) "map" else "function item"
+          new XPathException(expandMessage("Cannot atomize a " + thing), "FOTY0013")
+        } else {
+          new XPathException(
+            expandMessage(
+              "Cannot atomize an element that is defined in the schema to have element-only content"),
+            "FOTY0012")
+        }
       err.setIsTypeError(true)
       err.setLocation(getLocation)
       throw err
@@ -249,73 +201,69 @@ class Atomizer(sequence: Expression, role: RoleDiagnostic)
   }
 
   private def computeSingleValued(th: TypeHierarchy): Unit = {
-    val operandType: ItemType = getOperandItemType
+    val operandType = getOperandItemType
     if (th.relationship(operandType, ArrayItemType.ANY_ARRAY_TYPE) !=
       Affinity.DISJOINT) {
       singleValued = false
     } else {
       singleValued = untyped
-      if (!singleValued) {
+      if (! singleValued) {
         val nodeType: ItemType = getBaseExpression.getItemType
-        if (nodeType.isInstanceOf[NodeTest]) {
-          val st: SchemaType = nodeType.asInstanceOf[NodeTest].getContentType
-          if (st == Untyped.getInstance || st.isAtomicType || (st.isComplexType && st != AnyType.getInstance)) {
-            singleValued = true
-          }
-          if (!nodeType.getUType.overlaps(
-            UType.ELEMENT.union(UType.ATTRIBUTE))) {
-            singleValued = true
-          }
+        nodeType match {
+          case nodeTest: NodeTest =>
+            val st = nodeTest.getContentType
+            if (st == Untyped.getInstance || st.isAtomicType || (st.isComplexType && st != AnyType.getInstance))
+              singleValued = true
+            if (! nodeType.getUType.overlaps(
+              UType.ELEMENT.union(UType.ATTRIBUTE))) {
+              singleValued = true
+            }
+          case _ =>
         }
       }
     }
   }
 
   private def expandMessage(message: String): String =
-    if (roleDiagnostic == null) {
+    if (roleDiagnostic == null)
       message
-    } else {
+    else
       message + ". Found while atomizing the " + roleDiagnostic.getMessage
-    }
 
   override def optimize(visitor: ExpressionVisitor,
                         contextInfo: ContextItemStaticInfo): Expression = {
-    val exp: Expression = super.optimize(visitor, contextInfo)
-    if (exp == this) {
+    val exp = super.optimize(visitor, contextInfo)
+    if (exp eq this) {
       val th = visitor.getConfiguration.getTypeHierarchy
-      val operand: Expression = getBaseExpression
-      if (th.isSubType(operand.getItemType, BuiltInAtomicType.ANY_ATOMIC)) {
+      val operand = getBaseExpression
+      if (th.isSubType(operand.getItemType, BuiltInAtomicType.ANY_ATOMIC))
         return operand
-      }
-      if (operand.isInstanceOf[ValueOf] &&
-        !ReceiverOption.contains(operand.asInstanceOf[ValueOf].getOptions,
-          ReceiverOption.DISABLE_ESCAPING)) {
-        val cast: Expression =
-          operand.asInstanceOf[ValueOf].convertToCastAsString
-        cast.optimize(visitor, contextInfo)
-      }
-      if (operand.isInstanceOf[LetExpression] || operand
-        .isInstanceOf[ForExpression]) {
-        val action: Expression = operand.asInstanceOf[Assignation].getAction
-        operand
-          .asInstanceOf[Assignation]
-          .setAction(new Atomizer(action, roleDiagnostic))
-        operand.optimize(visitor, contextInfo)
-      }
-      if (operand.isInstanceOf[Choose]) {
-        operand.asInstanceOf[Choose].atomizeActions()
-        operand.optimize(visitor, contextInfo)
-      }
-      if (operand.isInstanceOf[Block]) {
-        val children: Array[Operand] = operand.asInstanceOf[Block].getOperanda
-        val atomizedChildren: Array[Expression] =
-          Array.ofDim[Expression](children.length)
-        for (i <- 0 until children.length) {
-          atomizedChildren(i) =
-            new Atomizer(children(i).getChildExpression, roleDiagnostic)
-        }
-        val newBlock: Block = new Block(atomizedChildren)
-        newBlock.typeCheck(visitor, contextInfo).optimize(visitor, contextInfo)
+      operand match {
+        case valueOf: ValueOf if ! ReceiverOption.contains(valueOf.getOptions, ReceiverOption.DISABLE_ESCAPING) =>
+          val cast = valueOf.convertToCastAsString
+          return cast.optimize(visitor, contextInfo)
+        case assignation @ (_: LetExpression | _: ForExpression) =>
+
+          // ORBEON: Would be nice if we could just call `assignation.getAction`, but the LUB is not computed, at
+          // least with Scala 2.13.
+
+          val action = operand.asInstanceOf[Assignation].getAction
+          operand
+            .asInstanceOf[Assignation]
+            .setAction(new Atomizer(action, roleDiagnostic))
+          return operand.optimize(visitor, contextInfo)
+        case choose: Choose =>
+          choose.atomizeActions()
+          return operand.optimize(visitor, contextInfo)
+        case block: Block =>
+          val children = block.getOperanda
+          val atomizedChildren: Array[Expression] =
+            Array.ofDim[Expression](children.length)
+          for (i <- children.indices)
+            atomizedChildren(i) = new Atomizer(children(i).getChildExpression, roleDiagnostic)
+          val newBlock: Block = new Block(atomizedChildren)
+          return newBlock.typeCheck(visitor, contextInfo).optimize(visitor, contextInfo)
+        case _ =>
       }
       if (untyped && operand.isInstanceOf[AxisExpression] &&
         operand.asInstanceOf[AxisExpression].getAxis == AxisInfo.ATTRIBUTE &&
@@ -323,8 +271,8 @@ class Atomizer(sequence: Expression, role: RoleDiagnostic)
           .asInstanceOf[AxisExpression]
           .getNodeTest
           .isInstanceOf[NameTest] &&
-        !operand.asInstanceOf[AxisExpression].isContextPossiblyUndefined) {
-        val name: StructuredQName =
+        ! operand.asInstanceOf[AxisExpression].isContextPossiblyUndefined) {
+        val name =
           operand.asInstanceOf[AxisExpression].getNodeTest.getMatchingNodeName
         val qName: FingerprintedQName =
           new FingerprintedQName(name, visitor.getConfiguration.getNamePool)
@@ -403,21 +351,20 @@ class Atomizer(sequence: Expression, role: RoleDiagnostic)
   }
 
   override def computeCardinality(): Int = {
-    val in: ItemType = getOperandItemType
-    val operand: Expression = getBaseExpression
+    val in = getOperandItemType
+    val operand = getBaseExpression
     if (singleValued) {
-      operand.getCardinality
+      return operand.getCardinality
     } else if (untyped && in.isInstanceOf[NodeTest]) {
-      operand.getCardinality
+      return operand.getCardinality
     } else if (Cardinality.allowsMany(operand.getCardinality)) {
-      StaticProperty.ALLOWS_ZERO_OR_MORE
+      return StaticProperty.ALLOWS_ZERO_OR_MORE
     } else if (in.isPlainType) {
-      operand.getCardinality
+      return operand.getCardinality
     } else if (in.isInstanceOf[NodeTest]) {
-      val schemaType: SchemaType = in.asInstanceOf[NodeTest].getContentType
-      if (schemaType.isAtomicType) {
-        operand.getCardinality
-      }
+      val schemaType = in.asInstanceOf[NodeTest].getContentType
+      if (schemaType.isAtomicType)
+        return operand.getCardinality
     }
     StaticProperty.ALLOWS_ZERO_OR_MORE
   }
