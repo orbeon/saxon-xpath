8821f55fc10d67373f77cdb23a16d6c43889ac0c
Erik Bruchez <erik@bruchez.org>
2020-11-28 13:36:37 -0800

    Fix `StringJoin` function

--- saxon/shared/src/main/scala/org/orbeon/saxon/functions/StringJoin.scala
+++ saxon/shared/src/main/scala/org/orbeon/saxon/functions/StringJoin.scala
@@ -1,14 +1,15 @@
 package org.orbeon.saxon.functions
 
 import org.orbeon.saxon.event.Outputter
-import org.orbeon.saxon.expr.{Expression, StaticProperty, XPathContext}
 import org.orbeon.saxon.expr.parser.{ContextItemStaticInfo, ExpressionVisitor}
+import org.orbeon.saxon.expr.{Expression, StaticProperty, XPathContext}
 import org.orbeon.saxon.model.BuiltInAtomicType
 import org.orbeon.saxon.om._
-import org.orbeon.saxon.tree.util.{CharSequenceConsumer, FastStringBuffer}
+import org.orbeon.saxon.tree.util.FastStringBuffer
 import org.orbeon.saxon.value.Cardinality
 
 import scala.beans.BooleanBeanProperty
+
 
 class StringJoin extends FoldingFunction with PushableFunction {
 
@@ -26,21 +27,17 @@ class StringJoin extends FoldingFunction with PushableFunction {
                                           visitor: ExpressionVisitor,
                                           contextInfo: ContextItemStaticInfo,
                                           arguments: Expression*): Expression = {
-    val e2: Expression =
-      super.makeOptimizedFunctionCall(visitor, contextInfo, arguments: _*)
-    if (e2 != null) return e2
-    val card: Int = arguments(0).getCardinality
-    if (!Cardinality.allowsMany(card)) {
-      if (Cardinality.allowsZero(card) ||
-        arguments(0).getItemType.getPrimitiveItemType != BuiltInAtomicType.STRING) {
-        SystemFunction.makeCall("string",
-          getRetainedStaticContext,
-          arguments(0))
-      } else {
+    val e2 = super.makeOptimizedFunctionCall(visitor, contextInfo, arguments: _*)
+    if (e2 != null)
+      return e2
+    val card = arguments(0).getCardinality
+    if (! Cardinality.allowsMany(card)) {
+      if (Cardinality.allowsZero(card) || arguments(0).getItemType.getPrimitiveItemType != BuiltInAtomicType.STRING)
+        SystemFunction.makeCall("string", getRetainedStaticContext, arguments(0))
+      else
         arguments(0)
-      }
-    }
-    null
+    } else
+      null
   }
 
   override def getFold(context: XPathContext,
@@ -54,22 +51,20 @@ class StringJoin extends FoldingFunction with PushableFunction {
   override def process(destination: Outputter,
                        context: XPathContext,
                        arguments: Array[Sequence]): Unit = {
-    val separator: CharSequence =
-      if (arguments.length > 1) arguments(1).head.getStringValueCS else ""
-    val output: CharSequenceConsumer = destination.getStringReceiver(false)
+    val separator = if (arguments.length > 1) arguments(1).head.getStringValueCS else ""
+    val output = destination.getStringReceiver(false)
     output.open()
-    var first: Boolean = true
-    val iter: SequenceIterator = arguments(0).iterate()
+    var first = true
+    val iter = arguments(0).iterate()
     var it: Item = null
     while ({
       it = iter.next()
       it
     } != null) {
-      if (first) {
+      if (first)
         first = false
-      } else {
+      else
         output.cat(separator)
-      }
       output.cat(it.getStringValueCS)
     }
     output.close()
@@ -79,9 +74,7 @@ class StringJoin extends FoldingFunction with PushableFunction {
     extends Fold {
 
     private var position: Int = 0
-
-    private var data: FastStringBuffer = new FastStringBuffer(
-      FastStringBuffer.C64)
+    private val data = new FastStringBuffer(FastStringBuffer.C64)
 
     def processItem(item: Item): Unit = {
       if (position == 0) {
@@ -99,10 +92,12 @@ class StringJoin extends FoldingFunction with PushableFunction {
         ZeroOrOne.empty
       else
         One.string(data.toString)
-
   }
 
-  override def equals(o: Any): Boolean = o.isInstanceOf[StringJoin] && super.equals(o) && returnEmptyIfEmpty == o.asInstanceOf[StringJoin].returnEmptyIfEmpty
+  override def equals(o: Any): Boolean =
+    o.isInstanceOf[StringJoin] &&
+      super.equals(o)          &&
+      returnEmptyIfEmpty == o.asInstanceOf[StringJoin].returnEmptyIfEmpty
+
   override def getCompilerName: String = "StringJoinCompiler"
-  override def call(context: XPathContext, args: Array[Sequence]): Sequence = call(context, args)
 }
