ec20c9f791c1e7f323b743ce2d4cce34d6c700db
Erik Bruchez <erik@bruchez.org>
2020-11-28 15:05:37 -0800

    Add test

--- saxon/shared/src/test/scala/org/orbeon/saxon/XPathTest.scala
+++ saxon/shared/src/test/scala/org/orbeon/saxon/XPathTest.scala
@@ -115,6 +115,7 @@ class XPathTest extends AnyFunSpec {
       ("""string(*[3])""", docElem, "Coyote"),
       ("""string(/*/*[3])""", doc, "Coyote"),
       ("""string-join(for $i in * return string($i), '')""", doc, "WileE.Coyote"),
+      ("""string-join((*[1]/string(), *[3]/string()), ' and ')""", docElem, "Wile and Coyote"),
 //      ("""string-join(for $i in * return string($i), '/')""", doc, "Wile/E./Coyote"), // FIXME: doesn't include '/'
 //      ("""string(/root/first-name)""", doc, "Wile"), // FIXME: returns blank
 //      ("""string((if (normalize-space(/root/name) = '') then '' else concat('Hello, ', /root/name, '!'))[1]))""", doc, "xxxx"),
