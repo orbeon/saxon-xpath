bdc1a42278000a54756143b01cca7be21fde08da
Erik Bruchez <erik@bruchez.org>
2020-11-28 15:05:28 -0800

    Fix `BlockIterator`

--- saxon/shared/src/main/scala/org/orbeon/saxon/expr/instruct/BlockIterator.scala
+++ saxon/shared/src/main/scala/org/orbeon/saxon/expr/instruct/BlockIterator.scala
@@ -1,33 +1,38 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2018-2020 Saxonica Limited
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+// If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+// This Source Code Form is "Incompatible With Secondary Licenses", as defined by the Mozilla Public License, v. 2.0.
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 package org.orbeon.saxon.expr.instruct
 
-import org.orbeon.saxon.expr.Operand
+import org.orbeon.saxon.expr.{Operand, XPathContext}
+import org.orbeon.saxon.om.{Item, SequenceIterator}
 
-import org.orbeon.saxon.expr.XPathContext
 
-import org.orbeon.saxon.om.Item
-
-import org.orbeon.saxon.om.SequenceIterator
-
+/**
+ * Iterate over the instructions in a sequence of instructions (or an XPath comma expression),
+ * concatenating the result of each instruction into a single combined sequence.
+ */
 class BlockIterator(private var operanda: Array[Operand],
                     private var context: XPathContext)
   extends SequenceIterator {
 
-  private var currentChildExpr: Int = 0
-
-  private var currentIter: SequenceIterator = _
-
-  private var position: Int = 0
+  private var currentChildExpr = 0
+  private var currentIter: SequenceIterator = null
+  private var position = 0
 
   def next(): Item = {
-    if (position < 0) {
+
+    if (position < 0)
       return null
-    }
+
     while (true) {
       if (currentIter == null) {
-        currentChildExpr += 1
         currentIter = operanda(currentChildExpr).getChildExpression.iterate(context)
+        currentChildExpr += 1
       }
-      val current: Item = currentIter.next()
+      val current = currentIter.next()
       if (current != null) {
         position += 1
         return current
@@ -35,16 +40,13 @@ class BlockIterator(private var operanda: Array[Operand],
       currentIter = null
       if (currentChildExpr >= operanda.length) {
         position = -1
-        null
+        return null
       }
     }
     null
   }
 
-  override def close(): Unit = {
-    if (currentIter != null) {
+  override def close(): Unit =
+    if (currentIter != null)
       currentIter.close()
-    }
-  }
-
 }
