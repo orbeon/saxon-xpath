874a92899d0146f2ffd81ff6eae0e4e65dda97b6
Erik Bruchez <erik@bruchez.org>
2020-12-01 15:17:28 -0800

    Fix missing `return`s and cleanup

--- saxon/shared/src/main/scala/org/orbeon/saxon/tree/tiny/TinyNodeImpl.scala
+++ saxon/shared/src/main/scala/org/orbeon/saxon/tree/tiny/TinyNodeImpl.scala
@@ -4,68 +4,55 @@
 // If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 // This Source Code Form is "Incompatible With Secondary Licenses", as defined by the Mozilla Public License, v. 2.0.
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+package org.orbeon.saxon.tree.tiny
+
+import java.util.function.Predicate
+
+import org.orbeon.saxon.model.{SchemaType, Type, UType}
+import org.orbeon.saxon.om.Genre.Genre
+import org.orbeon.saxon.om._
+import org.orbeon.saxon.pattern.{AnyNodeTest, NameTest, NodeTest}
+import org.orbeon.saxon.s9api.Location
+import org.orbeon.saxon.tree.NamespaceNode
+import org.orbeon.saxon.tree.iter.{AxisIterator, EmptyIterator, PrependAxisIterator}
+import org.orbeon.saxon.tree.tiny.TinyNodeImpl._
+import org.orbeon.saxon.tree.util.{FastStringBuffer, Navigator}
+import org.orbeon.saxon.utils.Configuration
+
+
 /**
   * A node in a TinyTree representing an XML element, character content, or attribute.
-  * <p>This is the top-level class in the implementation class hierarchy; it essentially contains
+  *
+  * This is the top-level class in the implementation class hierarchy; it essentially contains
   * all those methods that can be defined using other primitive methods, without direct access
-  * to data.</p>
+  * to data.
   *
   * @author Michael H. Kay
   */
-
-package org.orbeon.saxon.tree.tiny
-
-import org.orbeon.saxon.utils.Configuration
-import org.orbeon.saxon.model.SchemaType
-import org.orbeon.saxon.model.Type
-import org.orbeon.saxon.model.UType
-import org.orbeon.saxon.om._
-import org.orbeon.saxon.pattern.AnyNodeTest
-import org.orbeon.saxon.pattern.NameTest
-import org.orbeon.saxon.pattern.NodeTest
-import org.orbeon.saxon.s9api.Location
-import org.orbeon.saxon.tree.NamespaceNode
-import org.orbeon.saxon.tree.iter.AxisIterator
-import org.orbeon.saxon.tree.iter.EmptyIterator
-import org.orbeon.saxon.tree.iter.PrependAxisIterator
-import org.orbeon.saxon.tree.util.FastStringBuffer
-import org.orbeon.saxon.tree.util.Navigator
-import java.util.function.Predicate
-
-import TinyNodeImpl._
-import org.orbeon.saxon.om.Genre.Genre
-
 object TinyNodeImpl{
 
   /*@NotNull*/
-
   val NODE_LETTER: Array[Char] =
     Array('x', 'e', 'a', 't', 'x', 'x', 'x', 'p', 'c', 'r', 'x', 'x', 'x', 'n')
 
   def getParentNodeNr(tree: TinyTree, nodeNr: Int): Int = {
-    if (tree.depth(nodeNr) == 0) {
+    if (tree.depth(nodeNr) == 0)
       return -1
-    }
-    var p: Int = tree.next(nodeNr)
+    var p = tree.next(nodeNr)
     while (p > nodeNr) {
-      if (tree.nodeKind(p) == Type.PARENT_POINTER) {
-        tree.alpha(p)
-      }
+      if (tree.nodeKind(p) == Type.PARENT_POINTER)
+        return tree.alpha(p)
       p = tree.next(p)
     }
     p
   }
-
 }
 
 abstract class TinyNodeImpl extends NodeInfo {
 
    var tree: TinyTree = _
-
    var nodeNr: Int = _
-
   /*@Nullable*/
-
    var parent: TinyNodeImpl = null
 
   /**
@@ -90,7 +77,8 @@ abstract class TinyNodeImpl extends NodeInfo {
   /**
     * Get the type annotation of this node, if any. The type annotation is represented as
     * SchemaType object.
-    * <p>Types derived from a DTD are not reflected in the result of this method.</p>
+   *
+    * Types derived from a DTD are not reflected in the result of this method.
     *
     * @return For element and attribute nodes: the type annotation derived from schema
     *         validation (defaulting to xs:untyped and xs:untypedAtomic in the absence of schema
@@ -101,15 +89,13 @@ abstract class TinyNodeImpl extends NodeInfo {
     */
   override def getSchemaType: SchemaType = null
 
-  override def getColumnNumber(): Int = tree.getColumnNumber(nodeNr)
+  override def getColumnNumber: Int = tree.getColumnNumber(nodeNr)
 
-  def setSystemId(uri: String): Unit = {
+  def setSystemId(uri: String): Unit =
     tree.setSystemId(nodeNr, uri)
-  }
 
-   def setParentNode(parent: TinyNodeImpl): Unit = {
+   def setParentNode(parent: TinyNodeImpl): Unit =
     this.parent = parent
-  }
 
   override def isSameNodeInfo(other: NodeInfo): Boolean =
     (this eq other) ||
@@ -122,7 +108,6 @@ abstract class TinyNodeImpl extends NodeInfo {
   override def equals(other: Any): Boolean = other match {
     case other: NodeInfo => isSameNodeInfo(other)
     case _ => false
-
   }
 
   override def hashCode: Int =
@@ -140,7 +125,7 @@ abstract class TinyNodeImpl extends NodeInfo {
     * should not be saved for later use. The result of this operation holds the same location information,
     * but in an immutable form.
     */
-  def saveLocation(): Location = this
+  def saveLocation: Location = this
 
   def getSequenceNumber: Long = nodeNr.toLong << 32
 
@@ -158,218 +143,184 @@ abstract class TinyNodeImpl extends NodeInfo {
     * Ask whether this NodeInfo implementation holds a fingerprint identifying the name of the
     * node in the NamePool. If the answer is true, then the {@link #getFingerprint} method must
     * return the fingerprint of the node. If the answer is false, then the {@link #getFingerprint}
-    * method should throw an {@code UnsupportedOperationException}. In the case of unnamed nodes
+    * method should throw an `UnsupportedOperationException`. In the case of unnamed nodes
     * such as text nodes, the result can be either true (in which case getFingerprint should
     * return -1) or false (in which case getFingerprint may throw an exception).
     *
     * @return true if the implementation of this node provides fingerprints.
-    * @since 9.8; previously Saxon relied on using <code>FingerprintedNode</code> as a marker interface.
+    * @since 9.8; previously Saxon relied on using `FingerprintedNode` as a marker interface.
     */
   override def hasFingerprint: Boolean = true
 
   def getFingerprint: Int = {
-    val nc: Int = tree.nameCode(nodeNr)
-    if (nc == -1) {
+    val nc = tree.nameCode(nodeNr)
+    if (nc == -1)
       return -1
-    }
     nc & NamePool.FP_MASK
   }
 
   def getPrefix: String = {
-    val code: Int = tree.nameCode(nodeNr)
-    if (code < 0) {
+    val code = tree.nameCode(nodeNr)
+    if (code < 0)
       return ""
-    }
-    if (!NamePool.isPrefixed(code)) {
+    if (! NamePool.isPrefixed(code))
       return ""
-    }
     tree.prefixPool.getPrefix(code >> 20)
   }
 
   def getURI: String = {
-    val code: Int = tree.nameCode(nodeNr)
-    if (code < 0) {
+    val code = tree.nameCode(nodeNr)
+    if (code < 0)
       return ""
-    }
     tree.getNamePool.getURI(code & NamePool.FP_MASK)
   }
 
   def getDisplayName: String = {
-    val code: Int = tree.nameCode(nodeNr)
-    if (code < 0) {
+    val code = tree.nameCode(nodeNr)
+    if (code < 0)
       return ""
-    }
-    if (NamePool.isPrefixed(code)) {
+    if (NamePool.isPrefixed(code))
       getPrefix + ":" + getLocalPart
-    } else {
+    else
       getLocalPart
-    }
   }
 
   def getLocalPart: String = {
-    val code: Int = tree.nameCode(nodeNr)
-    if (code < 0) {
+    val code = tree.nameCode(nodeNr)
+    if (code < 0)
       return ""
-    }
     tree.getNamePool.getLocalName(code)
   }
 
   override def iterateAxis(axisNumber: Int): AxisIterator =
     if (axisNumber == AxisInfo.CHILD) {
-      if (hasChildNodes) {
+      if (hasChildNodes)
         new SiblingIterator(tree, this, null, true)
-      } else {
+      else
         EmptyIterator.ofNodes
-      }
     } else {
       iterateAxis(axisNumber, AnyNodeTest.getInstance)
     }
 
   def iterateAxis(axisNumber: Int,
                   predicate: Predicate[_ >: NodeInfo]): AxisIterator =
-    if (predicate.isInstanceOf[NodeTest]) {
-      val nodeTest: NodeTest = predicate.asInstanceOf[NodeTest]
-      val `type`: Int = getNodeKind
-      axisNumber match {
-        case AxisInfo.ANCESTOR => new AncestorIterator(this, nodeTest)
-        case AxisInfo.ANCESTOR_OR_SELF =>
-          var ancestors: AxisIterator = new AncestorIterator(this, nodeTest)
-          if (nodeTest.test(this)) {
-            new PrependAxisIterator(this, ancestors)
-          } else {
-            ancestors
-          }
-        case AxisInfo.ATTRIBUTE =>
-          if (`type` != Type.ELEMENT) {
-            EmptyIterator.ofNodes
-          }
-          if (tree.alpha(nodeNr) < 0) {
-            EmptyIterator.ofNodes
-          }
-          new AttributeIterator(tree, nodeNr, nodeTest)
-        case AxisInfo.CHILD =>
-          if (hasChildNodes) {
-            if (nodeTest.isInstanceOf[NameTest] &&
-              nodeTest.asInstanceOf[NameTest].getNodeKind == Type.ELEMENT) {
-              // fast path for common case
-              new NamedChildIterator(tree, this, nodeTest.getFingerprint)
+    predicate match {
+      case nodeTest: NodeTest =>
+        val `type` = getNodeKind
+        axisNumber match {
+          case AxisInfo.ANCESTOR =>
+            new AncestorIterator(this, nodeTest)
+          case AxisInfo.ANCESTOR_OR_SELF =>
+            val ancestors = new AncestorIterator(this, nodeTest)
+            if (nodeTest.test(this))
+              new PrependAxisIterator(this, ancestors)
+            else
+              ancestors
+          case AxisInfo.ATTRIBUTE =>
+            if (`type` != Type.ELEMENT)
+              EmptyIterator.ofNodes
+            else if (tree.alpha(nodeNr) < 0)
+              EmptyIterator.ofNodes
+            else
+              new AttributeIterator(tree, nodeNr, nodeTest)
+          case AxisInfo.CHILD =>
+            if (hasChildNodes)
+              nodeTest match {
+                case nameTest: NameTest if nameTest.getNodeKind == Type.ELEMENT =>
+                  new NamedChildIterator(tree, this, nodeTest.getFingerprint)
+                case _ =>
+                  new SiblingIterator(tree, this, nodeTest, true)
+              }
+            else
+              EmptyIterator.ofNodes
+          case AxisInfo.DESCENDANT =>
+            if (`type` == Type.DOCUMENT && nodeTest.isInstanceOf[NameTest] && nodeTest.getPrimitiveType == Type.ELEMENT) {
+              this.asInstanceOf[TinyDocumentImpl].getAllElements(nodeTest.getFingerprint)
+            } else if (hasChildNodes) {
+              if (nodeTest.getUType.overlaps(UType.TEXT))
+                new DescendantIterator(tree, this, nodeTest)
+              else
+                new DescendantIteratorSansText(tree, this, nodeTest)
             } else {
-              new SiblingIterator(tree, this, nodeTest, true)
+              EmptyIterator.ofNodes
             }
-          } else {
-            EmptyIterator.ofNodes
-          }
-        case AxisInfo.DESCENDANT =>
-          if (`type` == Type.DOCUMENT && nodeTest.isInstanceOf[NameTest] &&
-            nodeTest.getPrimitiveType == Type.ELEMENT) {
-            this
-              .asInstanceOf[TinyDocumentImpl]
-              .getAllElements(nodeTest.getFingerprint)
-          } else if (hasChildNodes) {
-            if (nodeTest.getUType.overlaps(UType.TEXT)) {
-              new DescendantIterator(tree, this, nodeTest)
+          case AxisInfo.DESCENDANT_OR_SELF =>
+            val descendants = iterateAxis(AxisInfo.DESCENDANT, nodeTest)
+            if (nodeTest.test(this))
+              new PrependAxisIterator(this, descendants)
+            else
+              descendants
+          case AxisInfo.FOLLOWING =>
+            if (`type` == Type.ATTRIBUTE || `type` == Type.NAMESPACE)
+              new FollowingIterator(tree, getParent, nodeTest, true)
+            else if (tree.depth(nodeNr) == 0)
+              EmptyIterator.ofNodes
+            else
+              new FollowingIterator(tree, this, nodeTest, false)
+          case AxisInfo.FOLLOWING_SIBLING =>
+            if (`type` == Type.ATTRIBUTE || `type` == Type.NAMESPACE || tree.depth(nodeNr) == 0)
+              EmptyIterator.ofNodes
+            else
+              new SiblingIterator(tree, this, nodeTest, false)
+          case AxisInfo.NAMESPACE =>
+            if (`type` != Type.ELEMENT)
+              EmptyIterator.ofNodes
+            else
+              NamespaceNode.makeIterator(this, nodeTest)
+          case AxisInfo.PARENT =>
+            val parent = getParent
+            Navigator.filteredSingleton(parent, nodeTest)
+          case AxisInfo.PRECEDING =>
+            if (`type` == Type.ATTRIBUTE || `type` == Type.NAMESPACE)
+              new PrecedingIterator(tree, getParent, nodeTest, false)
+            else if (tree.depth(nodeNr) == 0)
+              EmptyIterator.ofNodes
+            else
+              new PrecedingIterator(tree, this, nodeTest, false)
+          case AxisInfo.PRECEDING_SIBLING =>
+            if (`type` == Type.ATTRIBUTE || `type` == Type.NAMESPACE || tree.depth(nodeNr) == 0)
+              EmptyIterator.ofNodes
+            else
+              new PrecedingSiblingIterator(tree, this, nodeTest)
+          case AxisInfo.SELF => Navigator.filteredSingleton(this, nodeTest)
+          case AxisInfo.PRECEDING_OR_ANCESTOR =>
+            if (`type` == Type.DOCUMENT) {
+              EmptyIterator.ofNodes
+            } else if (`type` == Type.ATTRIBUTE || `type` == Type.NAMESPACE) {
+              val el = getParent
+              new PrependAxisIterator(
+                el,
+                new PrecedingIterator(tree, el, nodeTest, true))
             } else {
-              new DescendantIteratorSansText(tree, this, nodeTest)
+              new PrecedingIterator(tree, this, nodeTest, true)
             }
-          } else {
-            EmptyIterator.ofNodes
-          }
-        case AxisInfo.DESCENDANT_OR_SELF =>
-          var descendants: AxisIterator =
-            iterateAxis(AxisInfo.DESCENDANT, nodeTest)
-          if (nodeTest.test(this)) {
-            new PrependAxisIterator(this, descendants)
-          } else {
-            descendants
-          }
-        case AxisInfo.FOLLOWING =>
-          if (`type` == Type.ATTRIBUTE || `type` == Type.NAMESPACE) {
-            new FollowingIterator(tree, getParent, nodeTest, true)
-          } else if (tree.depth(nodeNr) == 0) {
-            EmptyIterator.ofNodes
-          } else {
-            new FollowingIterator(tree, this, nodeTest, false)
-          }
-        case AxisInfo.FOLLOWING_SIBLING =>
-          if (`type` == Type.ATTRIBUTE || `type` == Type.NAMESPACE ||
-            tree.depth(nodeNr) == 0) {
-            EmptyIterator.ofNodes
-          } else {
-            new SiblingIterator(tree, this, nodeTest, false)
-          }
-        case AxisInfo.NAMESPACE =>
-          if (`type` != Type.ELEMENT) {
-            EmptyIterator.ofNodes
-          }
-          NamespaceNode.makeIterator(this, nodeTest)
-        case AxisInfo.PARENT =>
-          var parent: NodeInfo = getParent
-          Navigator.filteredSingleton(parent, nodeTest)
-        case AxisInfo.PRECEDING =>
-          if (`type` == Type.ATTRIBUTE || `type` == Type.NAMESPACE) {
-            new PrecedingIterator(tree, getParent, nodeTest, false)
-          } else if (tree.depth(nodeNr) == 0) {
-            EmptyIterator.ofNodes
-          } else {
-            new PrecedingIterator(tree, this, nodeTest, false)
-          }
-        case AxisInfo.PRECEDING_SIBLING =>
-          if (`type` == Type.ATTRIBUTE || `type` == Type.NAMESPACE ||
-            tree.depth(nodeNr) == 0) {
-            EmptyIterator.ofNodes
-          } else {
-            new PrecedingSiblingIterator(tree, this, nodeTest)
-          }
-        case AxisInfo.SELF => Navigator.filteredSingleton(this, nodeTest)
-        case AxisInfo.PRECEDING_OR_ANCESTOR =>
-          if (`type` == Type.DOCUMENT) {
-            EmptyIterator.ofNodes
-          } else if (`type` == Type.ATTRIBUTE || `type` == Type.NAMESPACE) {
-            val el: TinyNodeImpl = getParent
-            new PrependAxisIterator(
-              el,
-              new PrecedingIterator(tree, el, nodeTest, true))
-          } else {
-            new PrecedingIterator(tree, this, nodeTest, true)
-          }
-        case _ =>
-          throw new IllegalArgumentException(
-            "Unknown axis number " + axisNumber)
-
-      }
-    } else {
-      new Navigator.AxisFilter(
-        iterateAxis(axisNumber, AnyNodeTest.getInstance),
-        predicate)
+          case _ =>
+            throw new IllegalArgumentException("Unknown axis number " + axisNumber)
+        }
+      case _ =>
+        new Navigator.AxisFilter(iterateAxis(axisNumber, AnyNodeTest.getInstance), predicate)
     }
 
   /*@Nullable*/
-
   def getParent: TinyNodeImpl = {
-    if (parent != null) {
+    if (parent != null)
       return parent
-    }
-    val p: Int = getParentNodeNr(tree, nodeNr)
-    if (p == -1) {
+    val p = getParentNodeNr(tree, nodeNr)
+    if (p == -1)
       return null
-    } else {
+    else
       parent = tree.getNode(p)
-    }
     null
   }
 
   def hasChildNodes: Boolean = false
-
   def getAttributeValue(uri: String, local: String): String = null
-
   def getRoot: NodeInfo = if (nodeNr == 0) this else tree.getRootNode
-
   override def getConfiguration: Configuration = tree.getConfiguration
 
   def getNamePool: NamePool = tree.getNamePool
 
   /*@Nullable*/
-
   def getDeclaredNamespaces(
                              buffer: Array[NamespaceBinding]): Array[NamespaceBinding] = null
 
@@ -393,64 +344,51 @@ abstract class TinyNodeImpl extends NodeInfo {
   }
 
   def isAncestorOrSelf(d: TinyNodeImpl): Boolean = {
-    if (tree != d.tree) {
+    if (tree != d.tree)
       return false
-    }
-    var dn: Int = d.nodeNr
+    var dn = d.nodeNr
     if (d.isInstanceOf[TinyAttributeImpl]) {
-      if (this.isInstanceOf[TinyAttributeImpl]) {
+      if (this.isInstanceOf[TinyAttributeImpl])
         return nodeNr == dn
-      } else {
+      else
         dn = tree.attParent(dn)
-      }
     }
-    if (this.isInstanceOf[TinyAttributeImpl]) {
+    if (this.isInstanceOf[TinyAttributeImpl])
       return false
-    }
-    if (nodeNr > dn) {
+    if (nodeNr > dn)
       return false
-    }
-    if (nodeNr == dn) {
+    if (nodeNr == dn)
       return true
-    }
-    if (!(this.isInstanceOf[TinyParentNodeImpl])) {
+    if (! this.isInstanceOf[TinyParentNodeImpl])
       return false
-    }
-    if (tree.depth(nodeNr) >= tree.depth(dn)) {
+    if (tree.depth(nodeNr) >= tree.depth(dn))
       return false
-    }
-    var n: Int = nodeNr
+    var n = nodeNr
     while (true) {
-      val nextSib: Int = tree.next(n)
-      if (nextSib < 0 || nextSib > dn) {
-        true
-      } else if (tree.depth(nextSib) == 0) {
-        true
-      } else if (nextSib < n) {
+      val nextSib = tree.next(n)
+      if (nextSib < 0 || nextSib > dn)
+        return true
+      else if (tree.depth(nextSib) == 0)
+        return true
+      else if (nextSib < n)
         n = nextSib
-      } else {
-        false
-      }
+      else
+        return false
     }
     false
   }
 
   override def isId: Boolean = false
-
-  override def isIdref(): Boolean = false
-
-  override def isNilled(): Boolean = tree.isNilled(nodeNr)
+  override def isIdref: Boolean = false
+  override def isNilled: Boolean = tree.isNilled(nodeNr)
 
   /**
     * Ask whether this is a node in a streamed document
     *
     * @return true if the node is in a document being processed using streaming
     */
-  override def isStreamed(): Boolean = false
+  override def isStreamed: Boolean = false
 
   def getTree: TinyTree = tree
-
   def getNodeNumber: Int = nodeNr
-
 }
-
