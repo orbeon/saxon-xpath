3c1e021c26224767f7c9cf167b287dd1c3924d86
Erik Bruchez <erik@bruchez.org>
2021-01-20 17:06:09 -0800

    Make case class for pattern matching

--- saxon/shared/src/main/scala/org/orbeon/saxon/ma/map/KeyValuePair.scala
+++ saxon/shared/src/main/scala/org/orbeon/saxon/ma/map/KeyValuePair.scala
@@ -4,14 +4,13 @@
 // If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 // This Source Code Form is "Incompatible With Secondary Licenses", as defined by the Mozilla Public License, v. 2.0.
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/**
-  * A key and a corresponding value to be held in a Map.
-  */
-
 package org.orbeon.saxon.ma.map
 
 import org.orbeon.saxon.om.GroundedValue
 import org.orbeon.saxon.value.AtomicValue
 
 
-class KeyValuePair(var key: AtomicValue, var value: GroundedValue)
+/**
+  * A key and a corresponding value to be held in a Map.
+  */
+case class KeyValuePair(key: AtomicValue, value: GroundedValue)
