693cfa6652c9e677550fe4e85e35454284619dcd
Erik Bruchez <erik@bruchez.org>
2021-01-28 21:38:30 -0800

    Cleanup

--- saxon/shared/src/main/scala/org/orbeon/saxon/functions/NamespaceUri_1.scala
+++ saxon/shared/src/main/scala/org/orbeon/saxon/functions/NamespaceUri_1.scala
@@ -1,30 +1,17 @@
 package org.orbeon.saxon.functions
 
 import org.orbeon.saxon.expr.XPathContext
+import org.orbeon.saxon.om.{Item, NodeInfo, One, ZeroOrOne}
+import org.orbeon.saxon.value.{AnyURIValue, AtomicValue}
 
-import org.orbeon.saxon.om.Item
-
-import org.orbeon.saxon.om.NodeInfo
-
-import org.orbeon.saxon.om.One
-
-import org.orbeon.saxon.om.ZeroOrOne
-
-import org.orbeon.saxon.trans.XPathException
-
-import org.orbeon.saxon.value.AnyURIValue
-
-import org.orbeon.saxon.value.AtomicValue
 
 class NamespaceUri_1 extends ScalarSystemFunction {
 
-  override def evaluate(item: Item, context: XPathContext): AtomicValue = {
-    val uri: String = item.asInstanceOf[NodeInfo].getURI
+  def evaluate(item: Item, context: XPathContext): AtomicValue = {
+    val uri = item.asInstanceOf[NodeInfo].getURI
     new AnyURIValue(uri)
   }
 
   override def resultWhenEmpty(): ZeroOrOne[Item] = new One(new AnyURIValue(""))
-
-  override def getCompilerName(): String = "NamespaceUriFnCompiler"
-
+  override def getCompilerName: String = "NamespaceUriFnCompiler"
 }
